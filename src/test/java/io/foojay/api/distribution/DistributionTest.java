7ccb8c0e2d86be10e194009d21eb6ad2a73c7151
Gerrit Grunwald <han.solo@mac.com>
2021-05-11 16:19:09 +0200

    fix

--- src/test/java/io/foojay/api/distribution/DistributionTest.java
+++ src/test/java/io/foojay/api/distribution/DistributionTest.java
@@ -27,6 +27,6 @@ public class DistributionTest {
 
     @Test
     public void distributions() {
-        assert (Distro.getDistributions().size() == 19);
+        assert (Distro.getDistributions().size() == 20);
     }
 }
