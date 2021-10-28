2b365f0d184e3c818b24d30b58f150496091d492
Gerrit Grunwald <han.solo@mac.com>
2021-10-28 17:09:36 +0200

    Added fpu class for hard- and soft-float

--- src/main/java/io/foojay/api/pkg/FPU.java
+++ src/main/java/io/foojay/api/pkg/FPU.java
@@ -19,8 +19,19 @@
 
 package io.foojay.api.pkg;
 
+import io.foojay.api.util.OutputFormat;
+
 import java.util.Arrays;
 import java.util.List;
+
+import static io.foojay.api.util.Constants.COLON;
+import static io.foojay.api.util.Constants.COMMA;
+import static io.foojay.api.util.Constants.COMMA_NEW_LINE;
+import static io.foojay.api.util.Constants.CURLY_BRACKET_CLOSE;
+import static io.foojay.api.util.Constants.CURLY_BRACKET_OPEN;
+import static io.foojay.api.util.Constants.INDENTED_QUOTES;
+import static io.foojay.api.util.Constants.NEW_LINE;
+import static io.foojay.api.util.Constants.QUOTES;
 
 
 public enum FPU implements ApiFeature {
@@ -50,6 +61,29 @@ public enum FPU implements ApiFeature {
 
     @Override public FPU[] getAll() { return values(); }
 
+    @Override public String toString(final OutputFormat outputFormat) {
+        StringBuilder msgBuilder = new StringBuilder();
+        switch(outputFormat) {
+            case FULL, REDUCED, REDUCED_ENRICHED -> {
+                msgBuilder.append(CURLY_BRACKET_OPEN).append(NEW_LINE)
+                          .append(INDENTED_QUOTES).append("name").append(QUOTES).append(COLON).append(QUOTES).append(name()).append(QUOTES).append(COMMA_NEW_LINE)
+                          .append(INDENTED_QUOTES).append("ui_string").append(QUOTES).append(COLON).append(QUOTES).append(uiString).append(QUOTES).append(COMMA_NEW_LINE)
+                          .append(INDENTED_QUOTES).append("api_string").append(QUOTES).append(COLON).append(QUOTES).append(apiString).append(QUOTES).append(NEW_LINE)
+                          .append(CURLY_BRACKET_CLOSE);
+            }
+            case FULL_COMPRESSED, REDUCED_COMPRESSED, REDUCED_ENRICHED_COMPRESSED, REDUCED_MINIMIZED -> {
+                msgBuilder.append(CURLY_BRACKET_OPEN)
+                          .append(QUOTES).append("name").append(QUOTES).append(COLON).append(QUOTES).append(name()).append(QUOTES).append(COMMA)
+                          .append(QUOTES).append("ui_string").append(QUOTES).append(COLON).append(QUOTES).append(uiString).append(QUOTES).append(COMMA)
+                          .append(QUOTES).append("api_string").append(QUOTES).append(COLON).append(QUOTES).append(apiString).append(QUOTES)
+                          .append(CURLY_BRACKET_CLOSE);
+            }
+        }
+        return msgBuilder.toString();
+    }
+
+    @Override public String toString() { return toString(OutputFormat.FULL_COMPRESSED); }
+
     public static FPU fromText(final String text) {
         if (null == text) { return NOT_FOUND; }
         switch (text) {
@@ -59,6 +93,8 @@ public enum FPU implements ApiFeature {
             case "HARD-FLOAT":
             case "hardfloat":
             case "HARDFLOAT":
+            case "hflt":
+            case "HFLT":
                 return HARD_FLOAT;
             case "soft_float":
             case "SOFT_FLOAT":
@@ -66,6 +102,8 @@ public enum FPU implements ApiFeature {
             case "SOFT-FLOAT":
             case "softfloat":
             case "SOFTFLOAT":
+            case "sflt":
+            case "SFLT":
                 return SOFT_FLOAT;
             case "unknown":
             case "UNKNOWN":
